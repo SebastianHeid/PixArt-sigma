8be458afe426c7f332b313a6a6225abe5d5eeb13
Sebastian Heid <sheid@hcigpu08>
2025-05-07 09:09:52 +0200

    Experiments

--- myScripts/run_train_lr5.sh
+++ myScripts/run_train_lr5.sh
@@ -1,11 +1,11 @@
 
-available_gpus=(2)
+available_gpus=(0)
 for gpu in "${available_gpus[@]}"; do
   export CUDA_VISIBLE_DEVICES=$gpu
  python -m torch.distributed.launch --nproc_per_node=1 --master_port=12341 \
           /export/home/sheid/PixArt-sigma/train_scripts/train.py \
-          /export/home/sheid/PixArt-sigma/configs/pixart_sigma_config/PixArt_sigma_xl2_img512_laion_lr5.py \
+          /export/home/sheid/PixArt-sigma/configs/pixart_sigma_config/PixArt_sigma_xl2_img512_laion_only_one_int_loss.py \
           --load-from /export/scratch/sheid/pixart/PixArt-Sigma-XL-2-512-MS.pth \
-          --work-dir /export/data/sheid/pixart/exp_lr5 \
+          --work-dir /export/data/sheid/pixart/PixArt_sigma_xl2_img512_laion_only_one_int_loss \
           --debug
 done
--- myScripts/run_train_lr6.sh
+++ myScripts/run_train_lr6.sh
@@ -1,11 +1,11 @@
 
-available_gpus=(3)
+available_gpus=(1)
 for gpu in "${available_gpus[@]}"; do
   export CUDA_VISIBLE_DEVICES=$gpu
  python -m torch.distributed.launch --nproc_per_node=1 --master_port=12340 \
           /export/home/sheid/PixArt-sigma/train_scripts/train.py \
-          /export/home/sheid/PixArt-sigma/configs/pixart_sigma_config/PixArt_sigma_xl2_img512_laion_lr6.py \
+          /export/home/sheid/PixArt-sigma/configs/pixart_sigma_config/PixArt_sigma_xl2_img512_laion_all_int_losses.py \
           --load-from /export/scratch/sheid/pixart/PixArt-Sigma-XL-2-512-MS.pth \
-          --work-dir /export/data/sheid/pixart/exp_lr6 \
+          --work-dir /export/data/sheid/pixart/epx_lr6_all_int_losses \
           --debug
 done
